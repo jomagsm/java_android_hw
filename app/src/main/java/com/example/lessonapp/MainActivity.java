b9fe8fad7da4823a32161650335122a66baaf71e
JomaGsm <jomagsm@gmail.com>
2023-01-27 14:20:10 +0600

    dz_7 finish

--- app/src/main/java/com/example/lessonapp/MainActivity.java
+++ app/src/main/java/com/example/lessonapp/MainActivity.java
@@ -17,60 +17,148 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Integer firstNumber, secondNumber;
+    private Boolean isOperationClick;
+
+    private View operationBtn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final String loginAndPassword = "admin";
-        TextView authAppBarTitle = findViewById(R.id.auth_app_bar_title);
-        EditText email = findViewById(R.id.auth_email);
-        EditText password = findViewById(R.id.auth_password);
-        Button button = findViewById(R.id.main_button);
-        TextView authDescription = findViewById(R.id.auth_description);
-        LinearLayout forgetPass = findViewById(R.id.forget_pass_text);
-        Drawable buttonDrawable = button.getBackground();
-        email.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (editable.length() > 0) {
-                    DrawableCompat.setTint(buttonDrawable, Color.rgb(248, 116, 0));
-                } else {
-                    DrawableCompat.setTint(buttonDrawable, Color.rgb(121, 121, 121));
-                }
-            }
-        });
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String result = "Неправильный логин и пароль";
-                String emailTxt = email.getText().toString();
-                System.out.println(emailTxt);
-                System.out.println("admin");
-                System.out.println(emailTxt.equals(loginAndPassword));
-                System.out.println(password.getText().toString());
-                if (email.getText().toString().equals(loginAndPassword) &&
-                        password.getText().toString().equals(loginAndPassword)) {
-                    result = "Добро пожаловать";
-                    authAppBarTitle.setVisibility(View.GONE);
-                    email.setVisibility(View.GONE);
-                    password.setVisibility(View.GONE);
-                    authDescription.setVisibility(View.GONE);
-                    button.setVisibility(View.GONE);
-                    forgetPass.setVisibility(View.GONE);
-                }
-                Toast.makeText(MainActivity.this,
-                        result, Toast.LENGTH_SHORT).show();
-            }
-        });
+        textView = findViewById(R.id.text_view);
     }
+
+    public void onNumberClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_one:
+                changeTextView("1");
+                break;
+            case R.id.btn_two:
+                changeTextView("2");
+                break;
+            case R.id.btn_three:
+                changeTextView("3");
+                break;
+            case R.id.btn_four:
+                changeTextView("4");
+                break;
+            case R.id.btn_five:
+                changeTextView("5");
+                break;
+            case R.id.btn_six:
+                changeTextView("6");
+                break;
+            case R.id.btn_seven:
+                changeTextView("7");
+                break;
+            case R.id.btn_eight:
+                changeTextView("8");
+                break;
+            case R.id.btn_nine:
+                changeTextView("9");
+                break;
+            case R.id.btn_zero:
+                changeTextView("0");
+                break;
+            case R.id.btn_dot:
+//                changeTextView(".");
+                break;
+            case R.id.btn_ac:
+                textView.setText("0");
+                firstNumber = null;
+                secondNumber = null;
+                break;
+        }
+        isOperationClick = false;
+    }
+
+    private void changeTextView(String value) {
+        if (textView.getText().toString().equals("0") || isOperationClick) {
+            textView.setText(value);
+        } else {
+            textView.append(value);
+        }
+    }
+
+    public void onOperationClick(View view) {
+        if (view.getId() != R.id.btn_equal) {
+            if (firstNumber == null) {
+                firstNumber = Integer.valueOf(textView.getText().toString());
+                isOperationClick = true;
+                operationBtn = view;
+                return;
+            }
+            if (secondNumber == null) {
+                System.out.println("FIRST " + firstNumber);
+                secondNumber = Integer.valueOf(textView.getText().toString());
+                Integer temp = getResult(firstNumber, secondNumber, operationBtn);
+                operationBtn = view;
+
+                if (temp != null) {
+                    firstNumber = temp;
+                    textView.setText(firstNumber.toString());
+                } else {
+                    textView.setText("ERROR");
+                }
+                System.out.println("FIRST " + firstNumber);
+
+            } else {
+                secondNumber = Integer.valueOf(textView.getText().toString());
+                Integer temp = getResult(firstNumber, secondNumber, operationBtn);
+                operationBtn = view;
+                if (temp != null) {
+                    firstNumber = temp;
+                    textView.setText(firstNumber.toString());
+                    ;
+                } else {
+                    textView.setText("ERROR");
+                }
+            }
+        } else {
+            if (secondNumber == null) {
+                secondNumber = Integer.valueOf(textView.getText().toString());
+                Integer result = getResult(firstNumber, secondNumber, operationBtn);
+                if (result != null) {
+                    textView.setText(result.toString());
+                } else {
+                    textView.setText("ERROR");
+                }
+            } else {
+                Integer temp = Integer.valueOf(textView.getText().toString());
+                Integer result = getResult(firstNumber, temp, operationBtn);
+                if (result != null) {
+                    textView.setText(result.toString());
+                } else {
+                    textView.setText("ERROR");
+                }
+            }
+
+        }
+        isOperationClick = true;
+    }
+
+    private Integer getResult(Integer a, Integer b, View operation) {
+        Integer result = null;
+        switch (operation.getId()) {
+            case R.id.btn_plus:
+                result = a + b;
+                break;
+            case R.id.btn_minus:
+                result = a - b;
+                break;
+            case R.id.btn_multiply:
+                result = a * b;
+                break;
+            case R.id.btn_division:
+                if (secondNumber != 0) {
+                    result = a / b;
+                }
+                break;
+        }
+        return result;
+    }
+
 }
